d76646bcc5a02033ce86536c7b71c77283ea017c
Isabel <isabel.moreno@genstudents.org>
2024-05-15 11:41:26 -0500

    data

---
+++ data.js
@@ -0,0 +1,50 @@
+export const sportswear = [
+    {
+        name: 'Top manga sisa cross negro',
+        url: 'https://fiber.com.co/cdn/shop/files/FEMT01A-5.jpg?v=1710691914',
+        description: 'Top deportivo de alta calidad, transpirable y cómodo para tus entrenamientos.',
+        id: 1
+    },
+    {
+        name: 'Top un solo hombro verde natural',
+        url: 'https://fiber.com.co/cdn/shop/files/TURT01B-5.jpg?v=1713679328',
+        description: 'Top deportivo con diseño moderno y ajuste perfecto para tus actividades deportivas.',
+        id: 2
+    },
+    {
+        name: 'Short running adjustable blanco',
+        url: 'https://fiber.com.co/cdn/shop/files/FEMS02B-5.jpg?v=1710691797',
+        description: 'Short deportivo ligero y elástico, ideal para correr o entrenar en el gimnasio.',
+        id: 3
+    },
+    {
+        name: 'Short waist ruching negro',
+        url: 'https://fiber.com.co/cdn/shop/files/FEMS01A-5.jpg?v=1710691509',
+        description: 'Short deportivo resistente al agua y de secado rápido, perfecto para actividades al aire libre.',
+        id: 4
+    },
+    {
+        name: 'Leggings rosa net saturn',
+        url: 'https://fiber.com.co/cdn/shop/files/TURL02A-5.jpg?v=1713676264',
+        description: 'Leggings deportivo con tejido suave y cálido, ideal para entrenamientos en todos los niveles.',
+        id: 5
+    },
+    {
+        name: 'Leggings cross waist ruching verde azul focused',
+        url: 'https://fiber.com.co/cdn/shop/files/CUSL01C-5.jpg?v=1708280680',
+        description: 'Prenda cómoda y útil para entrenamientos en todos los niveles ya sean al aire libre o en gimnasio.',
+        id: 6
+    },
+    {
+        name: 'Chaqueta gris lilac',
+        url: 'https://fiber.com.co/cdn/shop/files/FEMC02B-4.jpg?v=1710697515',
+        description: 'Chaqueta deportiva para antes y después del gimnasio.',
+        id: 7
+    },
+    {
+        name: 'Chaqueta negra femme',
+        url: 'https://fiber.com.co/cdn/shop/files/FEMC02A-5.jpg?v=1710697450',
+        description: 'Chaqueta deportiva transpirable, ideal para actividades al aire libre.',
+        id: 8
+    },
+];
